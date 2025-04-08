e48b2e46f22a7b63d70f6323c452306d96b8de09
Hong Jiarong <me@jrhim.com>
2025-04-08 10:45:55 +0800

    ci: update typst version specification in workflow and improve build script

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+if [ -f typst-package/rexllent.wasm ]; then
+    rm typst-package/rexllent.wasm
+fi
 rustup target add wasm32-wasi
 cargo build --release --target wasm32-wasi
 wasi-stub -r 0 ./target/wasm32-wasi/release/xlsx_parser_rs.wasm -o typst-package/rexllent.wasm
