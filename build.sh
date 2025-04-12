21b1b90e0845654031999631909f1c188f2e3a7a
Hong Jiarong <me@jrhim.com>
2025-04-12 22:49:48 +0800

    build: reorder commands in build script for clarity

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 if [ -f ./src/rexllent.wasm ]; then
     rm ./src/rexllent.wasm
 fi
-cd xlsx-parser-rs
 rustup target add wasm32-unknown-unknown
+cd xlsx-parser-rs
 cargo build --release --target wasm32-unknown-unknown
 cd ..
 wasm-opt ./xlsx-parser-rs/target/wasm32-unknown-unknown/release/xlsx_parser_rs.wasm -O3 --enable-bulk-memory -o ./src/rexllent.wasm
