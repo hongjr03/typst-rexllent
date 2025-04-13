cfee09de43b9c5bd3e5d0db9e77e42269b0cac38
Hong Jiarong <me@jrhim.com>
2025-04-13 11:01:48 +0800

    init go proj

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 if [ -f ./src/rexllent.wasm ]; then
     rm ./src/rexllent.wasm
 fi
-cd xlsx-parser-rs
-rustup target add wasm32-unknown-unknown
-cargo build --release --target wasm32-unknown-unknown
+cd xlsx-parser-go
+tinygo build -target=wasm-unknown -o rexllent.wasm
 cd ..
-wasm-opt ./xlsx-parser-rs/target/wasm32-unknown-unknown/release/xlsx_parser_rs.wasm -O3 --enable-bulk-memory -o ./src/rexllent.wasm
+wasm-opt ./xlsx-parser-go/rexllent.wasm -O3 --enable-bulk-memory -o ./src/rexllent.wasm
